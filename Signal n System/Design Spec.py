728eedd3a890cd499c00e41446ef8534892453ec
songmoro <97587585+songmoro@users.noreply.github.com>
2022-06-02 18:36:31 +0900

    .

--- Signal n System/Design Spec.py
+++ Signal n System/Design Spec.py
@@ -1,25 +1,37 @@
-from matplotlib.patches import Rectangle
+from __future__ import  division
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as signal
+from matplotlib.patches import Rectangle
+import scipy.signal
 
-M, beta = signal.fir_filter_design.kaiserord(delta, (fstop-fpass)/(fs/2.0))
+Ns =300 # number of samples
+N = 1024 # DFT size
 
-hn = signal.firwin(M,(fstop+fpass)/2.0, window = ('kaiser', beta), nyq = fs/2.0)
-w,H = signal.freqz(hn)
+fs = 1e3 # sample rate in Hz
+fpass = 100 # in Hz
+fstop = 150 # in Hz
+delta = 60 # in dB, desired attenuation in stopband
 
-fig, ax = subplots()
-fig.set_size_inches((8, 3))
+M,beta= scipy.signal.kaiserord(delta, (fstop-fpass)/(fs/2.))
 
-ax.plot(w/pi*fs/2.0, 20*log10(abs(H)))
-ax.set_xlabel("Frequency (Hz)", fontsize = 16)
-ax.set_ylabel(r"$20\log_{10} |H(f)| $", fontsize = 22)
-ymin, ymax = -80, 5
-ax.axis(ymin = ymin, ymax = ymax)
-ax.add_patch(Rectangle((0, ymin), width = fpass, height = ymax-ymin, color = 'g', alpha = 0.3))
-ax.add_patch(Rectangle((fpass, ymin), width = fstop-fpass, height = ymax-ymin, color = 'r', alpha = 0.3))
-ax.add_patch(Rectangle((fstop, ymin), width = fs/2-fstop, height = ymax-ymin, color = 'y', alpha = 0.3))
+hn = scipy.signal.firwin(M,(fstop+fpass)/2.,window=('kaiser',beta),nyq=fs/2.)
+w,H = scipy.signal.freqz(hn) # frequency response
+
+fig,ax = plt.subplots()
+fig.set_size_inches((8,3))
+
+ax.plot(w/np.pi*fs/2.,20*np.log10(abs(H)))
+ax.set_xlabel("frequency( Hz )",fontsize=16)
+ax.set_ylabel(r"$20\log_{10} |H(f)| $",fontsize=22)
+ymin,ymax = -80,5
+ax.axis(ymin = ymin,ymax=ymax)
+ax.add_patch(Rectangle((0,ymin),width=fpass,height=ymax-ymin,color='g',alpha=0.3))
+ax.add_patch(Rectangle((fpass,ymin),width=fstop-fpass,height=ymax-ymin,color='r',alpha=0.3))
+ax.add_patch(Rectangle((fstop,ymin),width=fs/2-fstop,height=ymax-ymin,color='y',alpha=0.3))
 ax.set_title("Number of taps=%d"%M)
-ax.text(10, -15, 'passband', fontsize = 14, bbox = dict(color = 'white'))
-ax.text(200, -15, 'stopband', fontsize = 16, bbox = dict(color = 'white'))
+ax.text(10,-15,'passband',fontsize=14,bbox=dict(color='white'))
+ax.text(200,-15,'stopband',fontsize=16,bbox=dict(color='white'))
 ax.grid()
+plt.tight_layout()
+plt.show()
+# fig.savefig('figure_00@.png', bbox_inches='tight', dpi=300)
--- Signal n System/Functions.py
+++ Signal n System/Functions.py
@@ -1,51 +1,70 @@
+from __future__ import division
+
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.linalg as sp
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import subplots
+from numpy import linspace, sin, pi, array, where, diff, argsort, sign
+from numpy.linalg import eig, eigvalsh, eigh
+
 
 def kernel(x,sigma=1):
     'convenient function to compute kernel of eigenvalue problem'
-    x = np.asanyarray(x) # ensure x is array
-    y = np.pi*np.where(x == 0,1.0e-20, x) # avoid divide by zero
-    return np.sin(sigma/2*y)/y
+    x = np.asanyarray(x)
+    y = pi*where(x == 0,1.0e-20, x)
+    return sin(sigma/2*y)/y
 
-nstep = 100 # quick and dirty integral quantization
-t = np.linspace(-1,1,nstep) # quantization of time
-dt = np.diff(t)[0] # differential step size
+nstep=100                # quick and dirty integral quantization
+t = linspace(-1,1,nstep) # quantization of time
+dt = diff(t)[0]          # differential step size
+def eigv(sigma):
+    return eigvalsh(kernel(t-t[:,None],sigma)).max() # compute max eigenvalue
+
+sigma = linspace(0.01,4,15) # range of time-bandwidth products to consider
+
+fig,ax = subplots()
+ax.plot(sigma, dt*array([eigv(i) for i in sigma]),'-o')
+ax.set_xlabel('time-bandwidth product $\sigma$',fontsize=14)
+ax.set_ylabel('max eigenvalue',fontsize=14)
+ax.axis(ymax=1.01)
+ax.grid()
+
+# fig.savefig('figure_00@.png', bbox_inches='tight', dpi=300)
 
 sigma=3
-w,v=sp.eigh(kernel(t-t[:,None],sigma))
+w,v=eigh(kernel(t-t[:,None],sigma))
 maxv=v[:, w.argmax()]
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig,ax=subplots()
 ax.plot(t,maxv)
-ax.set_xlabel('time',fontsize=18)
-ax.set_ylabel('$\psi_0(t)$',fontsize=22)
-ax.set_title('Eigenvector corresponding to e-value=%3.4f;$\sigma$=%3.2f'%(w.max()*dt,sigma))
+ax.set_xlabel('time',fontsize=14)
+ax.set_title('Eigenvector corresponding to e-value=%2.4e;$\sigma$=%3.2f'%(w.max()*dt,sigma))
+
+# fig.savefig('figure_00@.png', bbox_inches='tight', dpi=300)
 
 plt.tight_layout()
 plt.show()
 
+
 def kernel_tau(x,W=1):
     'convenient function to compute kernel of eigenvalue problem'
     x = np.asanyarray(x)
-    y = np.pi*np.where(x == 0,1.0e-20, x)
-    return np.sin(2*W*y)/y
+    y = pi*where(x == 0,1.0e-20, x)
+    return sin(2*W*y)/y
 
 nstep=300                # quick and dirty integral quantization
-t = np.linspace(-1,1,nstep) # quantization of time
-tt = np.linspace(-2,2,nstep)# extend interval
-
+t = linspace(-1,1,nstep) # quantization of time
+tt = linspace(-2,2,nstep)# extend interval
 sigma = 5
 W = sigma/2./2./t.max()
-
-w,v=sp.eig(kernel_tau(t-tt[:,None],W))
-ii = np.argsort(w.real)
+w,v=eig(kernel_tau(t-tt[:,None],5))
+ii = argsort(w.real)
 maxv=v[:, w.real.argmax()].real
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(tt,maxv/np.sign(maxv[nstep/2])) # normalize to keep orientation upwards
+fig,ax = subplots()
+ax.plot(tt,maxv)
+##plot(tt,v[:,ii[-2]].real)
 ax.set_xlabel('time',fontsize=14)
-ax.set_ylabel(r'$\phi_{max}(t)$',fontsize=18)
 ax.set_title('$\sigma=%d$'%(2*W*2*t.max()),fontsize=16)
 
+# fig.savefig('figure_00@.png', bbox_inches='tight', dpi=300)
+
+plt.tight_layout()
 plt.show()
